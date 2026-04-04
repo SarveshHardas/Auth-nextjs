45930ca083fb2d6962d6a750f50b7f79421a00b9
Sarvesh Hardas <hardassarvesh58@gmail.com>
2026-04-04 12:58:32 +0530

    Added model and signup post request for user

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,59 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+
+    if (!username) {
+      return NextResponse.json(
+        { message: "Username is required" },
+        { status: 400 },
+      );
+    }
+    if (!email) {
+      return NextResponse.json(
+        { message: "Email is required" },
+        { status: 400 },
+      );
+    }
+    if (!password) {
+      return NextResponse.json(
+        { message: "Password is required" },
+        { status: 400 },
+      );
+    }
+
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 },
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+
+    return NextResponse.json(
+      { message: "User created successfully", user: savedUser },
+      { status: 201 },
+    );
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
